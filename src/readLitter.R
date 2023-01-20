bee9828ca766dcedd23a5929b85d7fecffed9abb
Jochen Depestele <jochen.depestele@ilvo.vlaanderen.be>
2023-01-20 12:51:24 +0100

    initial run using BTS litter exchange data in NWW (ICES divs 7afg) and NS (ICES divs 4abc)

---
+++ src/readLitter.R
@@ -0,0 +1,211 @@
+###################################################################
+## Helper functions for handling DATRAS litter exchange data
+## Author: Casper W. Berg, DTU Aqua.
+###################################################################
+
+TVS2TVL <- function(){
+    ## Swept area (m^2) for 30 min hauls
+    TVS_TVL = c("TVLobs"=82798,"TVLobspred"=87163,"TVSobs"=68764,"TVSobspred"=68184)
+    TVS_TVL
+}
+
+readlitter <- function (file = "IBTS.csv", na.strings = c("-9", "-9.0", "-9.00", "-9.0000"), type="Weight"){
+
+    getExt<-function(x) { l=nchar(x); substr(x,l-3,l) }
+    if(getExt(file)==".zip"){
+        tempdir <- tempdir()
+        file <- unzip(file, exdir = tempdir)[1]
+    }
+    
+    ## Locating lines with headers
+    lines <- readLines(file)
+    i <- grep("RecordType", lines)
+    headers <- lines[i]
+    skip <- i - 1
+    nrow <- c(diff(i), 0) - 1
+    cat("Reading data files\n")
+    d <- lapply(1:length(i), function(i) {
+        emptyDF <- function(nm) {
+            structure(rep(list(logical(0)), length(nm)), .Names = nm, 
+                class = "data.frame")
+        }
+        if (nrow[i] == 0) {
+            return(emptyDF(strsplit(headers[i], ",")[[1]]))
+        }
+        
+        ans <- read.csv(file, nrow = nrow[i], 
+                                          skip = skip[i], na.strings = na.strings, stringsAsFactors = TRUE)
+        
+        ans$StNo <- as.character(ans$StNo)
+        ans <- renameDATRAS(ans)
+        ans
+    })
+    names(d) <- sapply(d, function(x) {
+        as.character(x$RecordType[1])
+    })
+
+    if (sum(sapply(d, nrow)) + length(i) != length(lines)) 
+        stop("csv file appears to be corrupt.")
+    if (is.null(d[[1]]$StatRec)) 
+        d[[1]]$StatRec <- d[[1]]$AreaCode
+    haul.id <- quote(factor(paste(Year, Quarter, Country, Ship, 
+                                  Gear, StNo, HaulNo, sep = ":")))
+
+    d[[1]]$haul.id = eval(haul.id,d[[1]])
+    d[[2]]$haul.id = eval(haul.id,d[[2]])
+    d[[2]]$haul.id = factor(d[[2]]$haul.id, levels=levels(d[[1]]$haul.id))
+    
+    LTnotHH = setdiff(levels(d[[2]]$haul.id),levels(d[[1]]$haul.id))
+    HHnotLT = setdiff(levels(d[[1]]$haul.id),levels(d[[2]]$haul.id))
+    if( length(LTnotHH) > 0 ){
+        print(paste("Dropping",length(LTnotHH),"LT records with no HH info:"))
+        print(LTnotHH)
+        d[[2]] = subset(d[[2]],!haul.id %in% LTnotHH)
+    }
+
+    if( length(HHnotLT) > 0 ){
+        print(paste("Dropping",length(HHnotLT),"HH records with no LT info:"))
+        print(LTnotHH)
+        d[[1]] = subset(d[[1]],!haul.id %in% HHnotLT)
+    }
+
+    
+    ## Zero litter hauls are recorded as RECO-LT and PARAM = LT-TOT...don't exclude!
+    badhauls = unique(d$LT$haul.id[ d$LT$LTREF=="RECO-LT" & d$LT$PARAM!="LT-TOT" ] )
+    if(length(badhauls)>0){
+        cat("Dropping ", length(badhauls), " hauls of type RECO-LT with PARAM!=LT-TOT\n")
+        d[[2]] = subset(d[[2]], !haul.id %in% badhauls)
+        d[[1]] = subset(d[[1]], !haul.id %in% badhauls)
+    }
+    
+        
+    if(type=="Numbers"){
+        badhauls = unique(d$LT$haul.id[ is.na(d$LT$LT_Items) ] )
+        if(length(badhauls)>0){ 
+            cat("Dropping ", length(badhauls), " hauls with only weight info\n")
+            d[[2]] = subset(d[[2]], !haul.id %in% badhauls)
+            d[[1]] = subset(d[[1]], !haul.id %in% badhauls)
+        }
+    }
+    ## zero hauls: weight unit does not matter
+    cat("Weight units:\n")
+    print(levels(d$LT$UnitWgt))
+    d$LT$UnitWgt <- factor(d$LT$UnitWgt,levels=c("g/haul","kg/haul"))
+    
+    zerohauls = d$LT$LT_Items==0 | d$LT$LT_Weight==0
+    d$LT$UnitWgt[ zerohauls ] = "kg/haul"
+        
+    if(type=="Weight"){
+        badhauls = unique(d$LT$haul.id[ is.na(d$LT$UnitWgt) | !d$LT$UnitWgt%in%c("g/haul","kg/haul")])
+        if(length(badhauls)>0){ 
+            cat("Dropping ", length(badhauls), " hauls with no or bad weight unit info\n")
+            d[[2]] = subset(d[[2]], !haul.id %in% badhauls)
+            d[[1]] = subset(d[[1]], !haul.id %in% badhauls)   
+        }
+        d$LT$LT_Weight[ d$LT$UnitWgt=="g/haul" ] = d$LT$LT_Weight[ d$LT$UnitWgt=="g/haul" ] / 1000
+    }
+    
+    if(nlevels(d[[1]]$haul.id) != nlevels(d[[2]]$haul.id)){
+        print("Missing haul info:")
+        cat("HH: " ,nlevels(d$HH$haul.id)," ,LT: ",nlevels(d$LT$haul.id),"\n","Setdiff:\n")
+        print( setdiff(levels(d[[1]]$haul.id),levels(d[[2]]$haul.id)))
+        stop()
+    }
+    
+    conv = read.csv("../data/litter_conversion.csv")
+    lookup <- function(x,table){
+        x <- factor(x)
+        levels(x) <- table[levels(x)]
+        if(is.numeric(table)) as.numeric(as.character(x)) else x
+    }
+
+    Code2Type <- structure(as.character(conv$Type), names=conv$C.TS)
+    Code2TypeRev <- structure(as.character(conv$Type), names=conv$C.TS.REV)
+    d[[2]] = transform(d[[2]],Type= factor(
+                      ifelse(as.character(LTREF)=="C-TS",
+                             as.character( lookup(PARAM,Code2Type) ),
+                             as.character( lookup(PARAM,Code2TypeRev) )
+                             )))
+
+    d[[2]]$Type <- factor(d[[2]]$Type,levels=c("Glass","Metal","Natural","Other","Plastic","Rubber"))
+                      
+    if(type=="Weight"){
+        agg = xtabs(LT_Weight ~ haul.id + Type,d[[2]])[ d[[1]]$haul.id, ]
+        
+    } else if(type=="Numbers"){
+        agg = xtabs(LT_Items ~ haul.id + Type,d[[2]])[ d[[1]]$haul.id, ]
+    }
+    dn = dimnames(agg)[2][[1]]
+    for(i in 1:length(dn))
+        d[[1]][,dn[i]] = agg[,i]
+
+    ## Extra categories:
+    ## 1. Singe use plastic (SUP)
+    Code2Type <- structure(as.character(conv$SUP), names=conv$C.TS)
+    Code2TypeRev <- structure(as.character(conv$SUP), names=conv$C.TS.REV)
+    d[[2]] = transform(d[[2]],SUP= factor(
+                                  ifelse(as.character(LTREF)=="C-TS",
+                                         as.character( lookup(PARAM,Code2Type) ),
+                                         as.character( lookup(PARAM,Code2TypeRev) )
+                                         )))
+    if(type=="Weight"){
+        agg = xtabs(LT_Weight ~ haul.id + SUP,d[[2]])[ d[[1]]$haul.id, ]
+        
+    } else if(type=="Numbers"){
+        agg = xtabs(LT_Items ~ haul.id + SUP,d[[2]])[ d[[1]]$haul.id, ]
+    }
+    d[[1]]$SUP = agg[ ,"Yes"]  
+         
+    ## 2. Fishing related
+    Code2Type <- structure(as.character(conv$Fishing.related), names=conv$C.TS)
+    Code2TypeRev <- structure(as.character(conv$Fishing.related), names=conv$C.TS.REV)
+    d[[2]] = transform(d[[2]],Fishing.related = factor(
+                                  ifelse(as.character(LTREF)=="C-TS",
+                                         as.character( lookup(PARAM,Code2Type) ),
+                                         as.character( lookup(PARAM,Code2TypeRev) )
+                                         )))
+    if(type=="Weight"){
+        agg = xtabs(LT_Weight ~ haul.id + Fishing.related,d[[2]])[ d[[1]]$haul.id, ]
+        
+    } else if(type=="Numbers"){
+        agg = xtabs(LT_Items ~ haul.id + Fishing.related,d[[2]])[ d[[1]]$haul.id, ]
+    }
+    d[[1]]$Fishing.related = agg[,"Yes"]
+
+    ## Extra variables (for surveyIndex package)
+    d[[1]]$lon = (d[[1]]$ShootLong + d[[1]]$HaulLong)/2
+    d[[1]]$lat = (d[[1]]$ShootLat + d[[1]]$HaulLat)/2
+    
+    d[[1]]$ctime = d[[1]]$Year + (d[[1]]$Month-1)/12 + (d[[1]]$Day-1)/365
+
+    d[[1]]$timeOfYear = (d[[1]]$Month-1)/12 + (d[[1]]$Day-1)/365
+    
+    d[[1]]$Year = factor(d[[1]]$Year)
+
+    d[[1]]$TimeShotHour = 12
+
+    TVS_TVL = TVS2TVL()
+    
+    d[[1]]$EFFORT = TVS_TVL[2]/30 * d[[1]]$HaulDur
+    d[[1]]$EFFORT[ d[[1]]$Gear == "TVS" ] = TVS_TVL[4]/30 * d[[1]]$HaulDur[ d[[1]]$Gear == "TVS" ]
+    
+    d[[1]]
+}
+
+
+
+mybubblePlot<-function (d, response = "HaulWgt", scale = NULL, col.zero = "red", 
+    pch.zero = "+", rim = FALSE, ...) 
+{
+    d$resp.var <- d[,response]
+    if (is.null(scale)) 
+        scale = 4/max(sqrt(d$resp.var), na.rm = TRUE)
+    plot(d$lon, d$lat, type = "n", xlab = "Longitude", ylab = "Latitude",...)
+    map("worldHires", fill = TRUE, plot = TRUE, add = TRUE, col = grey(0.5))
+    points(d$lon, d$lat, pch = 16, cex = scale * sqrt(d$resp.var))
+    if (rim) 
+        points(d$lon, d$lat, cex = scale * sqrt(d$resp.var), 
+            pch = 1, lwd = 0.5, col = "blue")
+    zero = subset(d, resp.var == 0)
+    points(zero$lon, zero$lat, pch = pch.zero, col = col.zero)
+}
---
+++ src/run.R
@@ -0,0 +1,486 @@
+############################################################################################
+## Script for calculating standardized indices of litter abundance in the Baltic Sea
+## using DATRAS Litter Exchange data.
+## Original script by Author: Casper W. Berg, DTU Aqua, June 2021
+## Minor modifications to apply to method to NWW and North Sea: Jochen Depestele (ILVO), Jan 2023
+#############################################################################################
+rm(list=ls())
+## remotes::install_github("DTUAqua/DATRAS/DATRAS")
+## NOTE: "posprob" branch of surveyIndex package needed
+# remotes::install_github("DTUAqua/DATRAS/DATRAS")
+# remotes::install_github("casperwberg/surveyIndex/surveyIndex",ref="posprob")
+
+library(DATRAS)
+library(maps); library(mapdata)
+library(surveyIndex)
+library(marmap)
+library(plot.matrix)
+library(xtable)
+
+source("readLitter.R")
+
+selreg <- c("NWW","NS")
+
+for (NR_RUNS in selreg){
+  litterTypes = c("Glass","Metal","Natural","Other","Plastic","Rubber")
+  print(NR_RUNS)
+  
+  litterTypesExt = c(litterTypes,"SUP","Fishing.related")
+  
+  setwd(file.path("C:/Users/jdepestele/OneDrive - ILVO/gitr/SEAwise/SEAwise_T4-5/BTS_litter/src"))
+  datafile = "../data/Litter Exchange Data_2023-01-19 10_58_43.zip"
+  
+  d = readlitter(datafile,type="Weight")
+  
+  d = subset(d,HaulDur>0 & HaulVal!="I")
+  d = subset(d,!is.na(d$lon) & !is.na(d$lat))
+  
+  d <- d[which(d$Year %in% c(2015:2021)),]
+  
+  xtabs(~Year,d)
+  xtabs(~Year+Quarter,d)
+  d$Year = factor(d$Year)
+
+  ## Make tables with data overview
+  makeTable<-function(x,fil,cap) cat(print(xtable( x, caption=cap, digits=0)),file=fil)
+
+  makeTable( xtabs(~Year+Quarter,d), fil=paste0("../report/",NR_RUNS,"/yqtab.tex"),cap="Weight: Number of hauls by year and quarter")
+  makeTable( xtabs(~Year+Country,d), fil=paste0("../report/",NR_RUNS,"/yctab.tex"),cap="Weight: Number of hauls by year and country")
+  makeTable( xtabs(~Gear+Country,d), fil=paste0("../report/",NR_RUNS,"/gctab.tex"),cap="Weight: Number of hauls by gear and country")
+  makeTable( xtabs(~Country+Quarter,d), fil=paste0("../report/",NR_RUNS,"/cqtab.tex"),cap="Weight: Number of hauls by country and quarter")
+
+
+
+  ## data frame to DATRASraw object
+  df2dr<-function(x){
+    x$haul.id = 1:nrow(x)
+    dd = list()
+    dd[[1]] = data.frame()
+    dd[[2]] = x
+    dd[[3]] = data.frame()
+    class(dd)<-"DATRASraw"
+    dd
+  }
+
+
+  drd = df2dr(d)
+
+  ## Get prediction grid
+  bgrid <- getBathyGrid(drd,minDepth=1,maxDepth=1000,maxDist=Inf,resolution=3,shapefile="../shapefiles/ICES/ICES_areas.shp",select="ICES_SUB")
+  # bgrid = subset(bgrid, ICES_SUB %in% c("VIIa","VIIf","VIIg"))
+
+  if(NR_RUNS=="NS"){
+    whichregion <- c("IVa","IVb","IVc")
+  }else{whichregion <- c("VIIa","VIIf","VIIg")}
+
+  bgrid = subset(bgrid, ICES_SUB %in% whichregion)
+
+  tmp = addSpatialData(df2dr(bgrid),shape="../shapefiles/EEZshape/EMODnet_HA_OtherManagementAreas_EEZ_v11_20210506.shp")
+  bgrid = tmp[[2]]
+  bgrid$Territory = factor(bgrid$Territory) ## drop empty factor levels
+
+  ## Plot bathy grid
+  my.palette<-colorRampPalette(c("darkblue","mediumblue","lightblue1"))
+  my.palette.vec=my.palette(100);
+  png(paste0("../output/",NR_RUNS,"/bathygrid.png"),width=1200,height=800)
+  plot(bgrid$lon,bgrid$lat,col=rev(my.palette.vec)[cut(bgrid$Depth,100)],pch=15,cex=1.3)
+  maps::map("worldHires", fill = TRUE, plot = TRUE,
+            add = TRUE, col = grey(0.5))
+  points(d$lon,d$lat,col=2,pch=".",cex=3)
+  dev.off()
+
+  ## Plot EEZ map
+  png(paste0("../output/",NR_RUNS,"/EEZmap.png"),width=1200,height=800)
+  plot(bgrid$lon,bgrid$lat,col=bgrid$Territory,pch=15,cex=1.3)
+  maps::map("worldHires", fill = TRUE, plot = TRUE,
+            add = TRUE, col = grey(0.9))
+  legend("bottomright",legend=levels(bgrid$Territory),col=1:nlevels(bgrid$Territory),pch=15,bg="white",cex=1.3)
+  dev.off()
+
+
+  ### Standardized effort: 1 km^2 (1e6 m^2).
+  ## Index is sum of grid points, so divide by number of grid points such that the index is
+  ## the mean litter abundance.
+  StdEffort = 1e6 / nrow(bgrid)
+
+
+  ## Define model formulas
+
+  NYEARS = length(unique(d$Year))
+
+
+  fm = paste0("s(ctime,k=",NYEARS,",bs='ds',m=c(1,0)) + s(lon,lat, bs='ds',m=c(1,0.5),k=128) + offset(log(EFFORT))")
+
+  ## Year effect instead of spline
+  fm2 = paste0("Year + s(lon,lat, bs='ds',m=c(1,0.5),k=128) + offset(log(EFFORT))")
+
+  ## Linear effect of time
+  fm3 = paste0("ctime + s(lon,lat, bs='ds',m=c(1,0.5),k=128) + offset(log(EFFORT))")
+
+  formulas = list(fm,fm2,fm3)
+
+
+  ages = 1
+
+  #################
+  ## Fit models
+  ##################
+
+  models = list()
+  models2 = list()
+  models3 = list()
+
+  for(lt in litterTypesExt){
+
+    cat("Doing ",lt,"...\n")
+
+    drd$Nage = matrix(d[,lt], nrow=nrow(d),ncol=1)
+    colnames(drd$Nage)<-1
+
+    system.time( models[[ lt ]]  <- getSurveyIdx(drd,ages,predD=bgrid,cutOff=0,fam="Tweedie",mc.cores=1,modelP=fm,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE,maxit=20) ) )
+
+    system.time( models2[[ lt ]]  <- getSurveyIdx(drd,ages,predD=bgrid,cutOff=0,fam="Tweedie",mc.cores=1,modelP=fm2,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE,maxit=20) ) )
+
+    system.time( models3[[ lt ]]  <- getSurveyIdx(drd,ages,predD=bgrid,cutOff=0,fam="Tweedie",mc.cores=1,modelP=fm3,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE,maxit=20) ) )
+
+
+  }
+
+
+  png(paste0("../output/",NR_RUNS,"/allmodels.png"),width=1200,height=800,pointsize=24)
+  par(mfrow=c(2,4))
+  for(i in 1:length(models)){
+    surveyIndex:::plot.SIlist(list(models[[i]],models2[[i]],models3[[i]]),main=names(models)[i])
+  }
+  dev.off()
+
+  ### Plotting
+  maxBubble = 8
+
+  for(lt in litterTypesExt){
+    png(paste0("../output/",NR_RUNS,"/",lt,"%03d.png"),width=1200,height=800)
+
+    ## Bubble plots - one per year
+    myscale=maxBubble/max(sqrt(d[,lt]))
+
+    xlims = range(d$lon)
+    ylims = range(d$lat)
+    op <- par(mfrow=n2mfrow(nlevels(d$Year)+1),mar=c(1,1,2,1),oma=c(0,0,2,0))
+    for(yy in sort(unique(d$Year))){
+      tmp = subset(d,Year==yy)
+      mybubblePlot(tmp,response=lt,scale=myscale,rim=TRUE,xlim=xlims,ylim=ylims,axes=FALSE)
+      box()
+      title(yy)
+    }
+    title(lt,outer=TRUE,line=0,cex.main=2)
+
+    legkg = c( 0, round((max(sqrt(d[,lt]))/maxBubble)^2,2),
+               round((max(sqrt(d[,lt])))^2,2))
+    legend("bottomright",pch=c(3,16,16),col=c(2,1,1),pt.cex=c(1,1,maxBubble),legend=paste(legkg,"kg"),bg="white",x.intersp=3,y.intersp=3)
+
+    ## All years together
+    mybubblePlot(d,response=lt,scale=myscale,rim=TRUE,xlim=xlims,ylim=ylims,axes=FALSE)
+    title("All years")
+
+    par(op)
+
+    ## Fitted map
+    surveyIdxPlots(models[[lt]],drd,myids=NULL,predD=bgrid,select="map",colors=rev(heat.colors(7)),legend=TRUE,legend.signif=2,map.cex=1.3,par=list(mfrow=c(1,1)),main=lt)
+
+
+    ## Fitted total abundance
+    surveyIndex:::plot.SIlist( list(models[[lt]]) ,main=paste(lt,"(kg / km^2)"))
+
+    dev.off()
+  }
+
+
+  ## Plot them all
+  png(paste0("../output/",NR_RUNS,"/allidx.png"),width=1200,height=800,pointsize=24)
+
+  allidxs = lapply(models[-3],function(x)x$idx)
+  maxY = max(sapply( allidxs,max))
+  par(mfrow=c(1,1),mar=c(4,4,3,3))
+  for(i in 1:length(allidxs)){
+    ys = rownames(allidxs[[i]])
+
+    if(i==1) plot(ys,allidxs[[i]],ylim=c(0,maxY),type="b",lwd=2,ylab="Index ( kg / km^2)") else lines(ys,allidxs[[i]],type="b",col=i,lwd=2)
+
+  }
+
+  legend("topright",col=1:length(allidxs),legend=names(allidxs),pch=1,lty=1,lwd=2)
+
+  dev.off()
+
+
+  ## Export model summaries
+
+  sink(paste0("../output/",NR_RUNS,"/summaries.txt"))
+  lapply(models,function(x) { summary(x$pModels[[1]])  } )
+  cat("=====================\n")
+  lapply(models2,function(x) { summary(x$pModels[[1]])  } )
+  cat("=====================\n")
+  lapply(models3,function(x) { summary(x$pModels[[1]])  } )
+  sink()
+
+
+  ################################
+  ## Calculate indices by EEZ
+  ################################
+  EEZmodels=list()
+
+  for(lt in litterTypesExt){
+    cat("Doing ",lt,"...\n")
+
+    EEZmodels[[lt]] = lapply(levels(bgrid$Territory),function(x){
+      cat(x,"\n")
+      pd = subset(bgrid,Territory==x)
+      redoSurveyIndex(drd,models[[lt]],predD=pd,myids=NULL,predfix=list(EFFORT=1e6/nrow(pd)))
+    })
+  }
+
+  ## Arrange indices and uncertainties in matrix form
+  EEZmat = matrix(NA,nlevels(bgrid$Territory),length(litterTypesExt))
+  rownames(EEZmat)<-levels(bgrid$Territory)
+  colnames(EEZmat)<-litterTypesExt
+  EEZmatCV <- EEZmat
+
+  for(lt in litterTypesExt){
+    for(ter in 1:nrow(EEZmat)){
+      i = which(litterTypesExt == lt)
+      EEZmat[ter,i] = tail(EEZmodels[[lt]][[ter]]$idx[,1],1)
+      logSD = (tail(log(EEZmodels[[lt]][[ter]]$up[,1]),1) - tail(log(EEZmodels[[lt]][[ter]]$lo[,1]),1))/4
+      EEZmatCV[ter,i] = logSD
+    }
+  }
+
+  col <- colorRampPalette(rev(c("red", "white", "blue")))
+  png(paste0("../output/",NR_RUNS,"/EEZplot%03d.png"),width=1200,height=800)
+
+  par(mfrow=c(1,1),mar=c(5,5,4,4))
+  plot(EEZmat,col=col,fmt.cell="%.2f",fmt.key="%.2f",las=1,xlab="",ylab="",main=paste("Litter density (kg / km^2) by EEZ",tail(levels(d$Year),1)))
+
+  col <- colorRampPalette(c("white","yellow","red"))
+  plot(EEZmatCV,col=col,fmt.cell="%.2f",fmt.key="%.2f",las=1,xlab="",ylab="",main=paste("Litter density uncertainty (CV) by EEZ",tail(levels(d$Year),1)))
+  dev.off()
+
+  
+  #####################################
+  ### Numbers insted of mass
+  #####################################
+  
+  d = readlitter(datafile,type="Numbers")
+  ## Note, different number of hauls when considering numbers!
+  d = subset(d,HaulDur>0 & HaulVal!="I")
+  d = subset(d,!is.na(d$lon) & !is.na(d$lat))
+  d <- d[which(d$Year %in% c(2015:2021)),]
+  d$Year = factor(d$Year)
+  drd = df2dr(d)
+  
+  
+  makeTable( xtabs(~Year+Quarter,d), fil=paste0("../report/",NR_RUNS,"/yqtab-n.tex"),cap="Numbers: Number of hauls by year and quarter")
+  makeTable( xtabs(~Year+Country,d), fil=paste0("../report/",NR_RUNS,"/yctab-n.tex"),cap="Numbers: Number of hauls by year and country")
+  makeTable( xtabs(~Gear+Country,d), fil=paste0("../report/",NR_RUNS,"/gctab-n.tex"),cap="Numbers: Number of hauls by gear and country")
+  makeTable( xtabs(~Country+Quarter,d), fil=paste0("../report/",NR_RUNS,"/cqtab-n.tex"),cap="Numbers: Number of hauls by country and quarter")
+  
+  
+  
+  StdEffort = 1e6 / nrow(bgrid)  ## Numbers pr km^2
+  
+  nmodels = list()
+  nmodels2 = list()
+  nmodels3 = list()
+  
+  for(lt in litterTypesExt){
+    
+    cat("Doing ",lt,"...\n")
+    
+    drd$Nage = matrix(d[,lt], nrow=nrow(d),ncol=1)
+    colnames(drd$Nage)<-1
+    
+    system.time( nmodels[[ lt ]]  <- getSurveyIdx(drd,ages,predD=bgrid,cutOff=0,fam="negbin",mc.cores=1,modelP=fm,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE) ) )
+    
+    system.time( nmodels2[[ lt ]]  <- getSurveyIdx(drd,ages,predD=bgrid,cutOff=0,fam="negbin",mc.cores=1,modelP=fm2,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE,maxit=20) ) )
+    
+    system.time( nmodels3[[ lt ]]  <- getSurveyIdx(drd,ages,predD=bgrid,cutOff=0,fam="negbin",mc.cores=1,modelP=fm3,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE,maxit=20) ) )
+    
+    
+  }
+  
+  
+  png(paste0("../output/",NR_RUNS,"/allmodels-numbers.png"),width=1200,height=800,pointsize=24)   
+  par(mfrow=c(2,4))
+  for(i in 1:length(models)){
+    surveyIndex:::plot.SIlist(list(nmodels[[i]],nmodels2[[i]],nmodels3[[i]]),main=names(models)[i])
+  }
+  dev.off()
+  
+  ## Maps
+  for(lt in litterTypesExt){
+    png(paste0("../output/",NR_RUNS,"/",lt,"-numbers.png"),width=1200,height=800)
+    
+    surveyIdxPlots(nmodels[[lt]],drd,myids=NULL,predD=bgrid,select="map",colors=rev(heat.colors(7)),legend=TRUE,legend.signif=2,map.cex=1.3,par=list(mfrow=c(1,1)),main=lt)
+    
+    dev.off()
+  }
+  
+  
+  
+  ## Output summaries
+  sink(paste0("../output/",NR_RUNS,"/summaries-numbers.txt"))
+  cat("============ Models on numbers ===============\n")
+  lapply(nmodels,function(x) { summary(x$pModels[[1]])  } )
+  cat("=====================\n")
+  lapply(nmodels2,function(x) { summary(x$pModels[[1]])  } )
+  cat("=====================\n")
+  lapply(nmodels3,function(x) { summary(x$pModels[[1]])  } )
+  sink()
+  
+  ##########################################
+  ## Calculate indices by EEZ (numbers)
+  ##########################################
+  EEZmodelsn=list()
+  
+  for(lt in litterTypesExt){
+    cat("Doing ",lt,"...\n")
+    
+    EEZmodelsn[[lt]] = lapply(levels(bgrid$Territory),function(x){
+      cat(x,"\n")
+      pd = subset(bgrid,Territory==x)
+      redoSurveyIndex(drd,nmodels[[lt]],predD=pd,myids=NULL,predfix=list(EFFORT=1e6/nrow(pd)))
+    })
+  }
+  
+  ## Arrange indices and uncertainties in matrix form
+  EEZmat = matrix(NA,nlevels(bgrid$Territory),length(litterTypesExt))
+  rownames(EEZmat)<-levels(bgrid$Territory)
+  colnames(EEZmat)<-litterTypesExt
+  EEZmatCV <- EEZmat
+  
+  for(lt in litterTypesExt){
+    for(ter in 1:nrow(EEZmat)){
+      i = which(litterTypesExt == lt)
+      EEZmat[ter,i] = tail(EEZmodelsn[[lt]][[ter]]$idx[,1],1)
+      logSD = (tail(log(EEZmodelsn[[lt]][[ter]]$up[,1]),1) - tail(log(EEZmodelsn[[lt]][[ter]]$lo[,1]),1))/4 
+      EEZmatCV[ter,i] = logSD
+    }
+  }
+  
+  col <- colorRampPalette(rev(c("red", "white", "blue")))
+  png(paste0("../output/",NR_RUNS,"/EEZplotn%03d.png"),width=1200,height=800)
+  
+  par(mfrow=c(1,1),mar=c(5,5,4,4))
+  plot(EEZmat,col=col,fmt.cell="%.2f",fmt.key="%.2f",las=1,xlab="",ylab="",main=paste("Litter density (numbers / km^2) by EEZ",tail(levels(d$Year),1)))
+  
+  col <- colorRampPalette(c("white","yellow","red"))
+  plot(EEZmatCV,col=col,fmt.cell="%.2f",fmt.key="%.2f",las=1,xlab="",ylab="",main=paste("Litter density uncertainty (CV) by EEZ (numbers)",tail(levels(d$Year),1)))
+  dev.off()
+  
+  
+  
+  #############################
+  ## Probability of encounter
+  #############################
+  
+  pmodels = list()
+  pmodels2 = list()
+  pmodels3 = list()
+  
+  ## We want probability pr haul
+  StdEffort = TVS2TVL()[2]
+  
+  for(lt in litterTypesExt){
+    
+    cat("Doing ",lt,"...\n")
+    
+    drd$Nage = matrix(d[,lt], nrow=nrow(d),ncol=1)
+    colnames(drd$Nage)<-1
+    
+    system.time( pmodels[[ lt ]]  <- getSurveyIdx(drd,ages,predD=bgrid,cutOff=0,fam="negbin",mc.cores=1,modelP=fm,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE), doProbs=TRUE ) )
+    
+    system.time( pmodels2[[ lt ]]  <- getSurveyIdx(drd,ages,predD=bgrid,cutOff=0,fam="negbin",mc.cores=1,modelP=fm2,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE,maxit=20), doProbs=TRUE ) )
+    
+    system.time( pmodels3[[ lt ]]  <- getSurveyIdx(drd,ages,predD=bgrid,cutOff=0,fam="negbin",mc.cores=1,modelP=fm3,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE,maxit=20), doProbs=TRUE ) )
+    
+    
+  }
+  
+  
+  png(paste0("../output/",NR_RUNS,"/allmodels-posprob.png"),width=1200,height=800,pointsize=24)   
+  par(mfrow=c(2,4))
+  for(i in 1:length(pmodels)){
+    surveyIndex:::plot.SIlist(list(pmodels[[i]],pmodels2[[i]],pmodels3[[i]]),main=names(pmodels)[i],posProb=TRUE)
+  }
+  dev.off()
+  
+  ## Fitted maps
+  
+  lastyear = tail(levels(d$Year),1)
+  
+  probcols = rev(hcl.colors(7,palette="Reds 3")) 
+  
+  for(lt in litterTypesExt){
+    png(paste0("../output/",NR_RUNS,"/",lt,"-posprob.png"),width=1200,height=800)
+    
+    surveyIdxPlots(pmodels[[lt]],drd,myids=NULL,predD=bgrid,select="absolutemap",colors=probcols,legend=TRUE,legend.signif=2,map.cex=1.3,par=list(mfrow=c(1,1),oma=c(0,0,1,0)),year=lastyear,posProb=TRUE,scaleMap=FALSE)
+    title(lt,outer=TRUE)
+    dev.off()
+    
+  }
+  
+  ##########################
+  ## CSV output
+  ##########################
+  
+  allout = list()
+  ## drop first year (2011) weight estimate in CSV output, because it is only available for weight
+  for(lt in litterTypesExt){
+    allout[[lt]] = data.frame(Type=lt, 
+                              Year = rownames(nmodels[[lt]]$idx), 
+                              DensityMass=models[[lt]]$idx[,1], DensityMassLow = models[[lt]]$lo[,1], DensityMassHigh=models[[lt]]$up[,1],
+                              DensityNumbers=nmodels[[lt]]$idx[,1], DensityNumbersLow = nmodels[[lt]]$lo[,1], DensityNumbersHigh=nmodels[[lt]]$up[,1],
+                              DensityProb=pmodels[[lt]]$idx0[,1],    DensityProbLow = pmodels[[lt]]$lo0[,1], DensityProbHigh=pmodels[[lt]]$up0[,1])
+  }
+  
+  
+  
+  allout.df = do.call(rbind,allout)
+  
+  write.csv(allout.df,file=paste0("../output/",NR_RUNS,"/litterEstimates.csv"),row.names=FALSE)
+  
+  
+  ############################################################
+  ## Trend models using only data from 2015 and onwards
+  ############################################################
+  
+  drd2 = subset(drd,!Year %in% as.character(2011:2014))
+  d2 = subset(d, !Year %in% as.character(2011:2014))
+  
+  StdEffort = 1e6 / nrow(bgrid)  ## Numbers/Mass pr km^2
+  
+  trend15models = list()
+  trend15modelsn = list()
+  
+  for(lt in litterTypesExt){
+    
+    cat("Doing ",lt,"...\n")
+    
+    drd2$Nage = matrix(d2[,lt], nrow=nrow(d2),ncol=1)
+    colnames(drd2$Nage)<-1
+    
+    system.time( trend15modelsn[[ lt ]]  <- getSurveyIdx(drd2,ages,predD=bgrid,cutOff=0,fam="negbin",mc.cores=1,modelP=fm3,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE,maxit=20) ) )
+    
+    system.time( trend15models[[ lt ]]  <- getSurveyIdx(drd2,ages,predD=bgrid,cutOff=0,fam="Tweedie",mc.cores=1,modelP=fm3,nBoot=1000, predfix=list(EFFORT=StdEffort),control=list(trace=TRUE,maxit=20) ) )
+    
+  }
+  
+  ## Output summaries
+  sink(paste0("../output/",NR_RUNS,"/trend15summaries.txt"))
+  cat("============ Trend models (2015 onwards)  ===============\n")
+  cat("============ Mass  ===============\n")
+  lapply(trend15models,function(x) { summary(x$pModels[[1]])  } )
+  cat("============ Numbers  ===============\n")
+  lapply(trend15modelsn,function(x) { summary(x$pModels[[1]])  } )
+  sink()
+  
+}
